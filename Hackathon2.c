7edcb2eda56936262732111902336c3b33465d6c
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2024-12-05 17:06:11 +0700

    Hackathon2.c

--- Hackathon2.c
+++ Hackathon2.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<stdlib.h> 
 int main(){
 	do{
-		int check=0;
-		int choice, n, temp, number, index, checkNumber;
+		int check;
+		int choice, n, temp, number, index, checkNumber, count3=0, count4=0;
 		int count=0, count1=0,count2=0;
-		int num[20];
+		int num[30];
 		printf("\n******MENU******\n");
 		printf("1. Nhap so phan tu va gia tri cho mang \n");
 		printf("2. In ra cac phan tu trong mang theo dang num[0], num[1],.. \n");
@@ -15,16 +15,17 @@ int main(){
 		printf("6. Xoa 1 phan tu tai vi tri cu the \n");
 		printf("7. Sap xep mang theo thu tu giam dan \n");
 		printf("8. Nhap vao 1 phan tu va tim kiem phan tu do trong mang \n");
-		printf("9. In ra toan bo so nguyen to trong mang da duoc binh phuong \n");
+		printf("9. In ra toan bo so nguyen to trong mang duoc binh phuong \n");
 		printf("10. Sap xep mang theo thu tu giam dan \n");
 		printf("11. Thoat \n");
 		printf("\t Lua chon cua ban la: ");
 		scanf("%d",&choice);
 		switch(choice){
 			case 1:
-				check=1;
+				 check = 1;
 				printf("Moi nhap so phan tu cua mang: ");
 				scanf("%d",&n);
+				
 				for(int i=0; i<n; i++){
 					printf("Moi nhap cac phan tu vao mang: ");
 					scanf("%d",&num[i]);
@@ -70,10 +71,6 @@ int main(){
 							num[j+1] = temp;
 						}
 					}
-				}
-				printf("Mang sau sap xep: \n");
-				for(int i=0; i<n; i++){
-					printf("%d\t",num[i]);
 				}
 				printf("\nPhan tu lon thu 2 trong mang la: %d",num[n-2]);
 				}else{
@@ -148,28 +145,50 @@ int main(){
 				//Nhap vao 1 phan tu va tim kiem phan tu do trong mang(Binary search)
 				printf("Nhap vao phan tu muon tim: ");
 				scanf("%d",&checkNumber);
-				int start=0, end=n-1, mid, flag=-1;
-				while(start<=end){
-					int mid=(start+end)/2;
-					if(num[mid]<checkNumber){
-						end=mid-1;
-					}else if(num[mid]>checkNumber){
-						start=mid+1;
-					}else{
-						flag = mid; 
-						printf("Phan tu %d co vi tri la %d ",checkNumber, mid+1);
-						return 0; 
+					int start=0, end = n-1 , mid, flag=-1;
+					while(start<=end){
+						int mid = (start+end)/2;
+						if(num[mid]>checkNumber){//chung to item phai nam ve ben trai, nho hon
+							end = mid-1;
+						}else if(num[mid]<checkNumber){//chung to item phai nam ve ben phai, lon hon
+							start = mid + 1;
+						}else{
+							flag = mid; 
+							printf("Phan tu %d co vi tri la %d ",checkNumber, mid+1);
+							break; 
+						}
 					}
+					if(flag==-1){
+						printf("Khong co phan tu %d trong mang",checkNumber); 
+					}		
+				}else{
+					printf("Mang chua co du lieu ");
 				}
-				if(flag==-1){
-					printf("Khong co phan tu %d trong mang",checkNumber); 
+			
+								
+				break;
+			case 9:
+				if(check==1){
+					//In ra so nguyen to
+				for(int i=0; i<n; i++){
+					count3=0;
+					for(int j=2; j<=num[i]; j++){
+						if(num[i]%j==0){
+							count3++;
+						}
+					}
+					if(count3==1){
+						printf("\nSo nguyen to: %d --> %d",num[i], num[i]*num[i]);
+						count4++;
+					}
+					
+				}
+				if(count4==0){
+					printf("\nKhong co so nguyen to trong mang ");
 				}
 				}else{
 					printf("Mang chua co du lieu ");
 				}
-								
-				break;
-			case 9:
 				break;
 			case 10:
 				if(check==1){
@@ -183,7 +202,7 @@ int main(){
 						}
 					}
 				}
-				printf("Mang su khi sap xep la: ");
+				printf("Mang sau khi sap xep la: \n");
 				for(int i=0; i<n; i++){
 					printf("%d\t",num[i]);
 					
@@ -196,6 +215,8 @@ int main(){
 			case 11:
 				exit(0);
 				break;
+			default:
+				printf("Default"); 
 		}
 		
 	}while(1);
